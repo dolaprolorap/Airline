b3da5d212256b835ad874c0f182c39110f78430e
boGGruz <bogdangruzkov@mail.ru>
2023-12-12 18:50:26 +1000

    added error alerts

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,24 +1,24 @@
 <template>
   <q-page class="row items-center justify-center">
     <q-card
-      class="q-pa-lg full-width column justify-center items-center"
-      style="max-width: 25%"
+        class="q-pa-lg full-width column justify-center items-center"
+        style="max-width: 25%"
     >
       <q-img src="logo.png" alt="Logo"/>
       <q-card-section class="full-width column" style="row-gap: 16px">
         <q-input
-          label="Username"
-          class="fit"
-          v-model="username"
-          outlined
-          dense
+            label="Username"
+            class="fit"
+            v-model="username"
+            outlined
+            dense
         />
         <q-input
-          label="Password"
-          class="fit"
-          v-model="password"
-          outlined
-          dense
+            label="Password"
+            class="fit"
+            v-model="password"
+            outlined
+            dense
         />
       </q-card-section>
       <q-card-section class="row full-width justify-between q-px-xl">
@@ -32,8 +32,20 @@
 <script setup lang="ts">
 import {ref} from 'vue';
 
-const username = ref('');
-const password = ref('');
+let username = '';
+let password = '';
+let errors: string[];
+function SubmitForm() {
+  errors = [];
+
+  if (username === '')
+    errors.push('The username is missing')
+
+  if (password === '')
+    errors.push('The password is missing')
+
+
+}
 </script>
 
 <style scoped lang="sass"></style>
