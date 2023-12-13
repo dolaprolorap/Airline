ec9aa14a2dba9d55aa75f20dab78662ed6f1df44
boGGruz <bogdangruzkov@mail.ru>
2023-12-13 18:05:26 +1000

    axios initionalization

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -7,14 +7,14 @@
       <q-img src="logo.png" alt="Logo"/>
       <q-card-section class="full-width column" style="row-gap: 16px">
         <q-input
-            ref="nameRef"
+            ref="usernameRef"
             label="Username"
             class="fit"
-            v-model="name"
+            v-model="username"
             outlined
             dense
             lazy-rules
-            :rules="nameRules"
+            :rules="usernameRules"
         />
         <q-input
             ref="ageRef"
@@ -28,28 +28,46 @@
         />
       </q-card-section>
       <q-card-section class="row full-width justify-between q-px-xl">
-        <q-btn class="col-4" color="primary" label="Log in"/>
-        <q-btn class="col-4" color="primary" label="Exit"/>
+        <q-btn @click="SubmitForm" class="col-4" color="primary" label="Log in"/>
+        <q-btn @click="Exit" class="col-4" color="primary" label="Exit"/>
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from 'vue'
+import axios from 'axios'
 
-const name = ref(null)
-const nameRef = ref(null)
+const username = ref(null)
+const usernameRef = ref(null)
 const password = ref(null)
 const passwordRef = ref(null)
 
-const nameRules = [
+const usernameRules = [
   (val?: string) => (val && val.length > 0) || 'Please enter username'
 ]
 const passwordRules = [
   (val?: string) => (val && val.length > 0) || 'Please enter password',
 ]
 
+function SubmitForm() {
+  const formData = {
+    username: username,
+    password: password
+  }
+  axios.post("/api/Auth/Login", formData)
+      .then(response => {
+        const msg = response.data.msg;
+        const accessToken = response.data.data.accessToken;
+        const refreshToken = response.data.data.refreshToken;
+
+      })
+}
+
+function Exit() {
+  return;
+}
 
 </script>
 
