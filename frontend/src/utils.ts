f374798fa6d491283204c1a411f65005bbe5ff08
ultragreed <agent002004@gmail.com>
2023-12-17 15:04:48 +1000

    utils redone to async

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -2,33 +2,35 @@ import { api } from 'boot/axios';
 import { LocalStorage } from 'quasar';
 import { useRouter } from 'vue-router';
 
-const getNewTokens = () => {
+export const getNewTokens = async () => {
   if (!LocalStorage.has('accessToken') || !LocalStorage.has('refreshToken')) {
     LocalStorage.set('accessToken', '');
     LocalStorage.set('refreshToken', '');
-
-    useRouter().push('/');
-
+    await useRouter().push('/');
     throw Error('Not authorized');
   }
-  console.log({
-    accessToken: LocalStorage.getItem('accessToken'),
-    refreshToken: LocalStorage.getItem('refreshToken')
-  })
 
-  api
+  return api
     .post('/Auth/Refresh', {
       accessToken: LocalStorage.getItem('accessToken'),
       refreshToken: LocalStorage.getItem('refreshToken')
     })
     .then(response => {
+      if (response.status !== 200) {
+        LocalStorage.set('accessToken', '');
+        LocalStorage.set('refreshToken', '');
+        useRouter().push('/').then(() => {
+          throw Error('Not authorized');
+        });
+      }
       const data = response.data;
+
       LocalStorage.set('accessToken', data.data.accessToken);
       LocalStorage.set('refreshToken', data.data.refreshToken);
     });
 };
-export const authPost = (url: string, data: object) => {
-  getNewTokens();
+export const authPost = async (url: string, data: object) => {
+  await getNewTokens();
 
   return api.post(url, data, {
     headers: {
@@ -37,8 +39,8 @@ export const authPost = (url: string, data: object) => {
   });
 };
 
-export const authGet = (url: string) => {
-  getNewTokens();
+export const authGet = async (url: string) => {
+  await getNewTokens();
 
   return api.get(url, {
     headers: {
