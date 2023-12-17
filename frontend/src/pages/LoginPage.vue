83b0fd998347059827e3c698c96d58e934795f84
ultragreed <agent002004@gmail.com>
2023-12-17 21:03:20 +1000

    login page redirect fix

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,21 +1,21 @@
 <script setup lang='ts'>
-import {ref} from 'vue';
-import {api} from 'src/boot/axios';
-import {useQuasar} from 'quasar'
+import { ref } from 'vue';
+import { api } from 'src/boot/axios';
+import { useQuasar } from 'quasar';
 
-import {useRouter} from 'vue-router';
+import { LocalStorage } from 'quasar';
+import { authGet } from 'src/utils';
 
-import {LocalStorage} from 'quasar';
-import {authGet} from 'src/utils';
+import { Router } from 'src/router';
 
 authGet('/Auth/GetMyself')
-    .then(response => {
-      const role = response.data.data.user.roleName;
-      if (role === 'Administrator')
-        router.replace({path: '/admin'});
-      if (role === 'User')
-        router.replace({path: '/user'});
-    });
+  .then(response => {
+    const role = response.data.data.user.roleName;
+    if (role === 'Administrator')
+      Router.replace({ path: '/admin' });
+    if (role === 'User')
+      Router.replace({ path: '/user' });
+  });
 
 const email = ref('');
 const password = ref('');
@@ -23,8 +23,6 @@ const password = ref('');
 const isPwd = ref(true);
 
 const error = ref('');
-
-const router = useRouter();
 
 const $q = useQuasar();
 
@@ -48,48 +46,50 @@ const submitForm = () => {
   }
 
   api.post('/Auth/Login', formData)
-      .then(response => {
+    .then(response => {
 
-        const msg = response.data.msg;
+      const msg = response.data.msg;
 
-        if (msg === 'UserNotFound') {
-          error.value = 'Wrong username or password';
-          return;
-        }
+      if (msg === 'UserNotFound') {
+        error.value = 'Wrong username or password';
+        return;
+      }
 
-        const accessToken = response.data.data.accessToken;
-        const refreshToken = response.data.data.refreshToken;
+      const accessToken = response.data.data.accessToken;
+      const refreshToken = response.data.data.refreshToken;
 
-        LocalStorage.set('accessToken', accessToken);
-        LocalStorage.set('refreshToken', refreshToken);
-        LocalStorage.set('email', email);
+      LocalStorage.set('accessToken', accessToken);
+      LocalStorage.set('refreshToken', refreshToken);
+      LocalStorage.set('email', email);
 
-        api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
-        $q.notify({
-          message: 'Successful log in!',
-          color: "green-4",
-          textColor: "white",
-          timeout: 1000
-        })
-        authGet('/Auth/GetMyself')
-            .then(response => {
-              const role = response.data.data.user.roleName;
-
-              if (role === 'Administrator')
-                router.push({path: '/admin'});
-              if (role === 'User')
-                router.push({path: '/user'});
-
-              LocalStorage.set('roleName', role);
-            });
+      $q.notify({
+        message: 'Successful log in!',
+        color: 'green-4',
+        textColor: 'white',
+        position: 'top',
+        timeout: 1000
       });
+      authGet('/Auth/GetMyself')
+        .then(response => {
+          const role = response.data.data.user.roleName;
+          console.log(role)
+
+          if (role === 'Administrator')
+            Router.push({ path: '/admin' });
+          if (role === 'User')
+            Router.push({ path: '/user' });
+
+          LocalStorage.set('roleName', role);
+        });
+    });
 };
 
 const closeBanner = () => {
   error.value = '';
   return;
-}
+};
 
 function exit() {
   return;
@@ -99,55 +99,55 @@ function exit() {
 <template>
   <q-page class='row items-center justify-center'>
     <q-card
-        class='q-pa-lg full-width column justify-center items-center'
-        style='max-width: 25%'
+      class='q-pa-lg full-width column justify-center items-center'
+      style='max-width: 25%'
     >
-      <q-img src='logo.png' alt='Logo'/>
+      <q-img src='logo.png' alt='Logo' />
       <q-card-section class='full-width column' style='row-gap: 16px'>
         <q-input
-            label='Email'
-            class='fit'
-            v-model='email'
-            outlined
-            dense
-            lazy-rules
-            :rules='usernameRules'
+          label='Email'
+          class='fit'
+          v-model='email'
+          outlined
+          dense
+          lazy-rules
+          :rules='usernameRules'
         />
         <q-input
-            v-model='password'
-            class='fit'
-            dense
-            outlined
-            lazy-rules
-            :rules='passwordRules'
-            :type="isPwd ? 'password' : 'text'"
-            label='Password'
+          v-model='password'
+          class='fit'
+          dense
+          outlined
+          lazy-rules
+          :rules='passwordRules'
+          :type="isPwd ? 'password' : 'text'"
+          label='Password'
         >
           <template v-slot:append>
             <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class='cursor-pointer'
-                @click='isPwd = !isPwd'
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class='cursor-pointer'
+              @click='isPwd = !isPwd'
             />
           </template>
         </q-input>
       </q-card-section>
-      <q-banner v-if='error !== ""' class="bg-red-1 text-white" style="border-radius: 10px">
+      <q-banner v-if='error !== ""' class='bg-red-1 text-white' style='border-radius: 10px'>
         <q-card-section
-            @submit.prevent='submitForm'
-            v-if='error !== ""'
-            class='row full-width justify-center text-negative'>
+          @submit.prevent='submitForm'
+          v-if='error !== ""'
+          class='row full-width justify-center text-negative'>
           {{ error }}
-          <q-btn flat size="0px"
-                 style="position: absolute; top: -8px; right: -15px; padding: 1px; border: 0px solid transparent;"
-                 @click="closeBanner">
-            <img src="public/black_crosss.png" style="width: 24px; height: 24px">
+          <q-btn flat size='0px'
+                 style='position: absolute; top: -8px; right: -15px; padding: 1px; border: 0px solid transparent;'
+                 @click='closeBanner'>
+            <img src='public/black_crosss.png' style='width: 24px; height: 24px'>
           </q-btn>
         </q-card-section>
       </q-banner>
       <q-card-section class='row full-width justify-between q-px-xl'>
-        <q-btn @click='submitForm' class='col-4' color='primary' label='Log in'/>
-        <q-btn @click='exit' class='col-4' color='primary' label='Exit'/>
+        <q-btn @click='submitForm' class='col-4' color='primary' label='Log in' />
+        <q-btn @click='exit' class='col-4' color='primary' label='Exit' />
       </q-card-section>
     </q-card>
   </q-page>
