5888c4fca3110935cd354de96721a40a2da0f88b
ultragreed <agent002004@gmail.com>
2023-12-17 18:00:46 +1000

    admin page office back, color distinction, disable login

--- frontend/src/pages/AdminPage.vue
+++ frontend/src/pages/AdminPage.vue
@@ -1,32 +1,48 @@
 <script setup lang='ts'>
 import { computed, ComputedRef, Ref, ref } from 'vue';
-import { QTableColumn } from 'quasar';
+import {QTableColumn } from 'quasar';
 
-import { authGet, authPost } from 'src/utils';
+import { authGet, authPost, getNewTokens } from 'src/utils';
+import { Router } from 'src/router';
 
-const office = ref('All offices');
+getNewTokens().then(() =>
+  authGet('/Auth/GetMyself')
+    .then(response => {
+      if (response.data.data.user.roleName === 'User')
+        Router.push('/user');
+    }
+  )
+);
 
-const officeOptions = ref([
-  'All offices',
-  'Abu Dhabi',
-  'Bahrain',
-  'Doha'
-]);
+interface Office {
+  country: string,
+  title: string,
+  phone: string,
+  contact: string
+}
+
+const selectedOffice = ref('All offices');
+
+const officeOptions: Ref<string[]> = ref(['All offices']);
+
+authGet('/Office/Get').then(
+  response => {
+    officeOptions.value = [
+      'All offices',
+      ...response.data.data.map((office: Office) => office.title)
+    ];
+  }
+);
 
 interface User {
   id: number,
-  roleId: number,
+  officeName: string,
   email: string,
-  password: string,
   firstName: string,
   lastName: string,
+  roleName: string,
   birthdate: string,
   active: boolean,
-  office: string,
-  role: string,
-  tickets: string[],
-  trackerrecords: string[],
-  token: string
 }
 
 interface Row {
@@ -34,9 +50,10 @@ interface Row {
   firstName: string,
   lastName: string,
   age: number,
-  role: string,
+  roleName: string,
   email: string,
-  office: string
+  officeName: string,
+  active: boolean
 }
 
 const selectedUsers: Ref<User[]> = ref([]);
@@ -56,7 +73,6 @@ const columns: QTableColumn[] = [
   },
   {
     name: 'firstName',
-    required: true,
     label: 'Name',
     field: 'name',
     align: 'left',
@@ -78,7 +94,7 @@ const columns: QTableColumn[] = [
     sort: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10)
   },
   {
-    name: 'userRole',
+    name: 'roleName',
     label: 'User Role',
     field: 'userRole',
     align: 'left',
@@ -92,7 +108,7 @@ const columns: QTableColumn[] = [
     sortable: true
   },
   {
-    name: 'office',
+    name: 'officeName',
     label: 'Office',
     field: 'office',
     align: 'left',
@@ -102,24 +118,30 @@ const columns: QTableColumn[] = [
 
 const visibleColumns = columns.filter(column => column.name !== 'id').map(column => column.name);
 
-const users: Ref<User[]> = ref([])
+const users: Ref<User[]> = ref([]);
+
+const isLoading = ref(true);
 
 authGet('/AdminPanel/Users').then(
   response => {
     users.value = response.data.data;
-    console.log(rows.value);
   }
-);
+).then(() => {
+  isLoading.value = false;
+});
+
 
 const rows: ComputedRef<Row[]> = computed(() => {
-  return users.value.map(user => {return {...user, age: calculateAge(user.birthdate)}})
+  return users.value.map(user => {
+    return { ...user, age: calculateAge(user.birthdate) };
+  });
 });
 
 
 const getSelectedRowsText = (amountOfRows: number) => `${amountOfRows} users selected.`;
 
 const submitChange = () => {
-  if (selectedUsers.value[0].role === newRole.value) {
+  if (selectedUsers.value[0].roleName === newRole.value) {
     return;
   }
 
@@ -129,15 +151,48 @@ const submitChange = () => {
       userEmail: selectedUsers.value[0].email,
       roleName: newRole.value
     });
-  selectedUsers.value[0].role = newRole.value;
+  selectedUsers.value[0].roleName = newRole.value;
 };
 
 const calculateAge = (birthday: string): number => {
-  const birthday_date = new Date(birthday)
+  const birthday_date = new Date(birthday);
   const ageDifMs = Date.now() - birthday_date.getTime();
   const ageDate = new Date(ageDifMs); // miliseconds from epoch
   return Math.abs(ageDate.getUTCFullYear() - 1970);
 };
+
+const determineUserColor = (user: Row) => {
+  if (!user.active)
+    return 'bg-red text-white';
+
+  if (user.roleName === 'Administrator')
+    return 'bg-green text-white';
+  else
+    return 'bg-white';
+};
+
+const filterRows = (
+  rows: Row[],
+  terms: string
+) => {
+  if (terms === 'All offices')
+    return rows;
+
+  return rows.filter(
+    row => row.officeName === terms
+  );
+};
+
+const changeLogin = () => {
+  for (let selectedUser of selectedUsers.value) {
+    const user = users.value.find(user => user.email === selectedUser.email) || selectedUser;
+    user.active = !user.active;
+    authPost( '/AdminPanel/SetActiveUser', {
+      email: user.email,
+      activeState: user.active
+    })
+  }
+}
 
 </script>
 
@@ -146,7 +201,7 @@ const calculateAge = (birthday: string): number => {
   >
     <q-card>
       <q-card-section class='column items-start' style='row-gap: 16px'>
-        <q-select v-model='office' :options='officeOptions' label='Office' dense outlined />
+        <q-select v-model='selectedOffice' :options='officeOptions' label='Office' dense outlined />
         <q-table
           style='max-height: 69vh'
           flat
@@ -155,7 +210,9 @@ const calculateAge = (birthday: string): number => {
           table-header-class='bg-primary'
           :rows='rows'
           :columns='columns'
-          :filter='office === "All offices" ? "" : office'
+          :filter='selectedOffice'
+          :filter-method='filterRows'
+          :loading='isLoading'
           row-key='id'
           selection='multiple'
           v-model:selected='selectedUsers'
@@ -169,7 +226,7 @@ const calculateAge = (birthday: string): number => {
             </q-th>
           </template>
           <template v-slot:body='props'>
-            <q-tr :props='props' @click='props.selected = !props.selected'>
+            <q-tr :props='props' @click='props.selected = !props.selected' :class='determineUserColor(props.row)'>
               <q-td>
                 <q-checkbox v-model='props.selected' color='secondary' />
               </q-td>
@@ -178,6 +235,9 @@ const calculateAge = (birthday: string): number => {
               </q-td>
             </q-tr>
           </template>
+          <template v-slot:loading>
+            <q-inner-loading showing color='primary' />
+          </template>
         </q-table>
         <div class='row fit justify-start'>
           <q-btn
@@ -185,19 +245,30 @@ const calculateAge = (birthday: string): number => {
             style='font-size: medium'
             color='primary'
             :disable='selectedUsers.length !== 1'
-            @click='changeRoleDialog = true; newRole = selectedUsers[0].role'
+            @click='changeRoleDialog = true; newRole = selectedUsers[0].roleName'
           >Change role
           </q-btn>
-          <q-btn class='tex-gyre-adventor-bold col-3 offset-1' style='font-size: medium' color='primary'>Enable/Disable
-            Login
+          <q-btn
+            class='tex-gyre-adventor-bold col-3 offset-1'
+            style='font-size: medium'
+            color='primary'
+            @click='changeLogin'
+            :disable='selectedUsers.length === 0'
+          >
+            Enable/Disable Login
           </q-btn>
-          <q-btn class='tex-gyre-adventor-bold col-2 offset-4' style='font-size: medium' color='primary'>Add user
+          <q-btn
+            class='tex-gyre-adventor-bold col-2 offset-4'
+            style='font-size: medium'
+            color='primary'
+          >
+            Add user
           </q-btn>
         </div>
       </q-card-section>
     </q-card>
     <q-dialog v-model='changeRoleDialog'>
-      <q-card style='min-width: 350px'>
+      <q-card class='q-pa-sm' style='min-width: 350px'>
         <q-card-section>
           <div class='text-h6'>Change role</div>
         </q-card-section>
@@ -206,7 +277,7 @@ const calculateAge = (birthday: string): number => {
           <q-input dense v-model='selectedUsers[0].email' readonly label='Email' type='email' />
           <q-input dense v-model='selectedUsers[0].firstName' readonly label='Name' />
           <q-input dense v-model='selectedUsers[0].lastName' readonly label='Last name' />
-          <q-input dense v-model='selectedUsers[0].office' readonly label='Office' />
+          <q-input dense v-model='selectedUsers[0].officeName' readonly label='Office' />
           <div class='column q-mt-md'>
             Role:
             <q-radio v-model='newRole' val='Administrator' label='Administrator' />
