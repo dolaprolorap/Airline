7f4571c2ac4d93b22a4d984a593e25235acdd647
boGGruz <bogdangruzkov@mail.ru>
2023-12-18 11:51:25 +1000

    added navigation buttons

--- frontend/src/layouts/AuthedLayout.vue
+++ frontend/src/layouts/AuthedLayout.vue
@@ -1,31 +1,36 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import { exit } from 'src/utils';
 import { LocalStorage } from 'quasar';
+import AdminPage from 'pages/AdminPage.vue';
 
 const roleName = LocalStorage.getItem('roleName');
-
-
-
 </script>
 
 <template>
-  <q-layout view='hHh Lpr fFf'> <!-- Be sure to play with the Layout demo on docs -->
+  <q-layout view="hHh Lpr fFf">
+    <!-- Be sure to play with the Layout demo on docs -->
     <!-- (Optional) The Header -->
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-          <q-btn flat :to='roleName === "Administrator" ? "/admin" : "/user"'>
-            <q-img src='logo_white.png' fit='scale-down' width='170px' />
+          <q-btn flat :to="roleName === 'Administrator' ? '/admin' : '/user'">
+            <q-img src="logo_white.png" fit="scale-down" width="170px" />
           </q-btn>
+          <q-btn style="font-size: large" flat v-if="roleName==='Administrator'" :to="'/admin'">
+            Admin panel
+          </q-btn>
+          <q-btn style="font-size: large" flat :to="'/user'"> User panel</q-btn>
+          <q-btn style="font-size: large" flat :to="'/flightmanager'"> flight schedule</q-btn>
+          <q-btn style="font-size: large" flat :to="'/book-flights'"> book flight </q-btn>
         </q-toolbar-title>
         <q-btn
           round
-          class='q-mr-lg'
-          color='white'
-          text-color='primary'
-          icon='logout'
-          to='/'
-          @click='exit'
+          class="q-mr-lg"
+          color="white"
+          text-color="primary"
+          icon="logout"
+          to="/"
+          @click="exit"
         />
       </q-toolbar>
     </q-header>
@@ -34,7 +39,5 @@ const roleName = LocalStorage.getItem('roleName');
       <!-- This is where pages get injected -->
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
-
