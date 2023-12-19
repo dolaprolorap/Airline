2e4ab25993ccd7784595ea3dd714e1192bf05860
boGGruz <bogdangruzkov@mail.ru>
2023-12-20 02:15:15 +1000

    amenties page done

---
+++ frontend/src/pages/AmentiesReport.vue
@@ -0,0 +1,195 @@
+<template>
+  <q-page class="row items-center justify-center" style="">
+    <q-card>
+      <q-card-section class="row justify-end text-h3"
+        >Amenties Report
+      </q-card-section>
+      <q-splitter horizontal></q-splitter>
+      <q-card-section class="column items-start" style="row-gap: 16px">
+        <q-table
+          style="max-height: 69vh"
+          flat
+          hide-pagination
+          bordered
+          dense
+          separator="vertical"
+          table-header-class="bg-primary"
+          :rows="rows"
+          :columns="columns"
+        >
+          <template v-slot:header-cell="props">
+            <q-th
+              v-if="props.col.name !== 'id'"
+              :props="props"
+              class="text-white tex-gyre-adventor-bold"
+              style="font-size: large"
+            >
+              <div v-html="multiLineHeader(props.col.label)"></div>
+            </q-th>
+          </template>
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td
+                v-for="col in visibleColumns"
+                :key="col"
+                :props="props"
+                style="font-size: medium"
+              >
+                {{ props.row[col] }}
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { authGet } from 'src/utils';
+
+const columns = ref([
+  {
+    name: 'ExtraBlanket',
+    label: 'Extra Blanket',
+    field: 'ExtraBlanket',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'NextSeatFree',
+    label: 'Next Seat Free',
+    field: 'NextSeatFree',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'TwoNeighboringSeatsFree',
+    label: 'Two Neighboring Seats Free',
+    field: 'TwoNeighboringSeatsFree',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'TabletRental',
+    label: 'Tablet Rental',
+    field: 'TabletRental',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'LaptopRental',
+    label: 'Laptop Rental',
+    field: 'LaptopRental',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'LoungeAccess',
+    label: 'Lounge Access',
+    field: 'LoungeAccess',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'SoftDrinks',
+    label: 'Soft Drinks',
+    field: 'SoftDrinks',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'PremiumHeadphonesRental',
+    label: 'Premium Headphones Rental',
+    field: 'PremiumHeadphonesRental',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'ExtraBag',
+    label: 'Extra Bag',
+    field: 'ExtraBag',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'FastCheckinLane',
+    label: 'Fast Checkin Lane',
+    field: 'FastCheckinLane',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'WiFi50mb',
+    label: 'Wi-Fi 50 mb',
+    field: 'WiFi50mb',
+    required: true,
+    align: 'center',
+  },
+  {
+    name: 'WiFi250mb',
+    label: 'Wi-Fi 250 mb',
+    field: 'WiFi250mb',
+    required: true,
+    align: 'center',
+  },
+]);
+
+const multiLineHeader = (label: any) => {
+  // Replace spaces with <br> to create line breaks
+  return label.replace(/\s+/g, '<br>');
+};
+
+const visibleColumns = ref([
+  'ExtraBlanket',
+  'NextSeatFree',
+  'TwoNeighboringSeatsFree',
+  'TabletRental',
+  'LaptopRental',
+  'LoungeAccess',
+  'SoftDrinks',
+  'PremiumHeadphonesRental',
+  'ExtraBag',
+  'FastCheckinLane',
+  'WiFi50mb',
+  'WiFi250mb',
+]);
+
+const rows = ref<
+  Array<{
+    ExtraBlanket: string;
+    NextSeatFree: string;
+    TwoNeighboringSeatsFree: string;
+    TabletRental: string;
+    LaptopRental: string;
+    LoungeAccess: string;
+    SoftDrinks: string;
+    PremiumHeadphonesRental: string;
+    ExtraBag: string;
+    FastCheckinLane: string;
+    WiFi50mb: string;
+    WiFi250mb: string;
+  }>
+>([]);
+
+onMounted(async () => {
+  await authGet('/Amenities/MakeReport').then((response) => {
+    const tableArray = response.data.data;
+    rows.value.push({
+      ExtraBlanket: tableArray['Extra Blanket'],
+      NextSeatFree: tableArray['Next Seat Free'],
+      TwoNeighboringSeatsFree: tableArray['Two Neighboring Seats Free'],
+      TabletRental: tableArray['Tablet Rental'],
+      LaptopRental: tableArray['Laptop Rental'],
+      LoungeAccess: tableArray['Lounge Access'],
+      SoftDrinks: tableArray['Soft Drinks'],
+      PremiumHeadphonesRental: tableArray['Premium Headphones Rental'],
+      ExtraBag: tableArray['Extra Bag'],
+      FastCheckinLane: tableArray['Fast Checkin Lane'],
+      WiFi50mb: tableArray['Wi-Fi 50 mb'],
+      WiFi250mb: tableArray['Wi-Fi 250 mb']
+    });
+  });
+});
+</script>
--- frontend/src/pages/PurchaseAmenties.vue
+++ frontend/src/pages/PurchaseAmenties.vue
@@ -1,21 +1,157 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref, Ref } from 'vue';
+import { authGet, authPost } from 'src/utils';
 
 const bookingRef = ref('');
 
-const firstAvailibleFlight = ref('');
-const availibleFlightsList = ref(['dsa', 'dsa', 'dsa']);
+const availibleFlightsList = ref([]);
+const firstFlight = ref('');
 
-const fullName = ref('dsadsa');
-const passportNumber = ref('dsads');
-const cabinClass = ref('dasd');
+const fullName = ref('');
+const cabinClass = ref('');
 
+const flightsArray = ref<
+  Array<{
+    id: string;
+    flightNumber: string;
+    fromAirportName: string;
+    toAirportName: string;
+    date: string;
+    time: string;
+    firstname: string;
+    lastname: string;
+    phone: string;
+    cabinTypeName: string;
+  }>
+>([]);
+
+const serviceNames = ref<
+  Array<{
+    id: string;
+    service: string;
+    price: string;
+  }>
+>([]);
+
+const id1 = ref(0);
 const submitBookingRef = () => {
-  return;
+  authGet(`/Amenities/GetTicketsByBookRef?bookRef=${bookingRef.value}`).then(
+    (response) => {
+      const tableArray = response.data.data;
+      flightsArray.value = tableArray;
+      tableArray.forEach((tuple: any) => {
+        const str =
+          tuple.id +
+          ', ' +
+          tuple.flightNumber +
+          ', ' +
+          tuple.fromAirportName +
+          '-' +
+          tuple.toAirportName +
+          ', ' +
+          tuple.date +
+          ', ' +
+          tuple.time;
+        availibleFlightsList.value.push(str);
+      });
+      firstFlight.value = availibleFlightsList.value[0];
+    }
+  );
+};
+const submitFlight = () => {
+  flightsArray.value.forEach((tuple: any) => {
+    id1.value = Number(tuple.id);
+    if (tuple.id === id1.value) {
+      fullName.value = tuple.firstname + ' ' + tuple.lastname;
+      cabinClass.value = tuple.cabinTypeName;
+    }
+  });
 };
 
-const submitFlight = () => {
-  return;
+const itemsSelected = ref(0);
+const totalPayable = ref(0);
+
+onMounted(async () => {
+  await authGet('/Amenities/GetAmenities').then((response) => {
+    const tableArray = response.data.data;
+    tableArray.forEach((tuple: any) => {
+      serviceNames.value.push({
+        id: tuple.id,
+        service: tuple.service,
+        price: tuple.price,
+      });
+    });
+  });
+});
+
+const extra = ref(false);
+const next = ref(false);
+const neighbour = ref(false);
+const tablet = ref(false);
+const laptop = ref(false);
+const lounge = ref(false);
+const soft = ref(false);
+const premium = ref(false);
+const extraBag = ref(false);
+const fast = ref(false);
+const mb50 = ref(false);
+const mb250 = ref(false);
+
+const checkBoxArray: Ref<boolean>[] = [];
+checkBoxArray.push(
+  extra,
+  next,
+  neighbour,
+  tablet,
+  laptop,
+  lounge,
+  soft,
+  premium,
+  extraBag,
+  fast,
+  mb50,
+  mb250
+);
+
+const submitChecks = () => {
+  for (let i = 0; i < checkBoxArray.length; i++) {
+    if (checkBoxArray[i].value) {
+      itemsSelected.value += 1;
+      totalPayable.value += Number(serviceNames.value[i].price);
+      console.log('dsa', serviceNames.value[i].price);
+    }
+  }
+};
+
+const reset = () => {
+  for (let i = 0; i < checkBoxArray.length; i++) {
+    checkBoxArray[i].value = false;
+  }
+  totalPayable.value = 0;
+  itemsSelected.value = 0;
+};
+let array =[];
+const id2 = Number(id1.value);
+const confirm = () => {
+  if (!itemsSelected.value){ return;}
+  let arrayNames = [];
+  for (let i = 0; i < checkBoxArray.length; i++) {
+    if (checkBoxArray[i].value) {
+      arrayNames.push(serviceNames.value[i].service);
+    }
+  }
+  const formData = {
+    ticketId: id1.value,
+    amenityNamesToApply: arrayNames,
+  };
+  authPost('/Amenities/ApplyAmenities', formData);
+  for (let i = 0; i < checkBoxArray.length; i++) {
+    if (checkBoxArray[i].value) {
+      checkBoxArray[i].value = false;
+    }
+  }
+  itemsSelected.value = 0;
+  totalPayable.value = 0;
 };
 </script>
 
@@ -26,8 +162,7 @@ const submitFlight = () => {
         >Purchase Amenties
       </q-card-section>
       <q-splitter horizontal></q-splitter>
-      <q-card-section class="row justify-start text-h6 q-mt-sm q-mx-md"
-        >
+      <q-card-section class="row justify-start text-h6 q-mt-sm q-mx-lg">
         <span>Booking reference:</span>
         <q-input
           class="q-mx-lg"
@@ -45,11 +180,11 @@ const submitFlight = () => {
         </q-btn>
       </q-card-section>
       <q-separator class="q-mt-sm" inset />
-      <q-card-section class="row justify-start text-h6 q-mt-sm q-mx-md">
+      <q-card-section class="row justify-start text-h6 q-mt-sm q-mx-lg">
         <span>Choose your flights:</span>
         <q-select
           class="q-mx-lg q-mt-none"
-          v-model="firstAvailibleFlight"
+          v-model="firstFlight"
           :options="availibleFlightsList"
           label="Airport list"
           dense
@@ -63,22 +198,12 @@ const submitFlight = () => {
         </q-btn>
       </q-card-section>
       <q-separator class="q-mt-sm" inset />
-      <q-card-section class="column justify-start text-h6 q-mx-md">
+      <q-card-section class="row justify-between text-h6 q-mx-md">
         <q-card-section class="row">
           <span>Full name:</span>
           <q-input
             class="q-mx-lg"
             v-model="fullName"
-            dense
-            readonly
-            style="width: 30%"
-          />
-        </q-card-section>
-        <q-card-section class="row">
-          <span>Passport number:</span>
-          <q-input
-            class="q-mx-lg"
-            v-model="passportNumber"
             dense
             readonly
             style="width: 30%"
@@ -96,8 +221,108 @@ const submitFlight = () => {
         </q-card-section>
       </q-card-section>
       <q-separator class="q-mt-md" inset />
+      <q-card-section class="row text-primary justify-end text-h6">
+        <span>AMONIC Airlines Amenties</span>
+      </q-card-section>
       <q-card-section class="row">
-        <q-card-section></q-card-section>
+        <q-card-section class="column">
+          <q-checkbox
+            v-model="extra"
+            class="text-h6"
+            label="Extra Blanket ($10)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="next"
+            class="text-h6"
+            label="Next Seat Free ($30)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="neighbour"
+            class="text-h6"
+            label="Two Neighboring Seats Free ($50)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="tablet"
+            class="text-h6"
+            label="Tablet Rental ($12)"
+          ></q-checkbox>
+        </q-card-section>
+        <q-card-section class="column">
+          <q-checkbox
+            v-model="laptop"
+            class="text-h6"
+            label="Laptop Rental ($15)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="lounge"
+            class="text-h6"
+            label="Lounge Access ($25)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="soft"
+            class="text-h6"
+            label="Soft Drinks ($0)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="premium"
+            class="text-h6"
+            label="Premium Headphones Rental ($5)"
+          ></q-checkbox>
+        </q-card-section>
+        <q-card-section class="column">
+          <q-checkbox
+            v-model="extraBag"
+            class="text-h6"
+            label="Extra Bag ($15)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="fast"
+            class="text-h6"
+            label="Fast Checkin Lane ($10)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="mb50"
+            class="text-h6"
+            label="Wi-Fi 50 mb ($0)"
+          ></q-checkbox>
+          <q-checkbox
+            v-model="mb250"
+            class="text-h6"
+            label="Wi-Fi 250 mb ($25)"
+          ></q-checkbox>
+        </q-card-section>
+      </q-card-section>
+      <q-card-section class="row justify-between">
+        <q-btn
+          class="tex-gyre-adventor-bold col-2 q-mx-md"
+          style="font-size: medium; width: 30%"
+          @click="reset"
+          color="primary"
+          >reset
+        </q-btn>
+        <q-btn
+          class="tex-gyre-adventor-bold col-2 q-mx-md"
+          style="font-size: medium; width: 30%"
+          @click="submitChecks"
+          color="primary"
+          >submit
+        </q-btn>
+      </q-card-section>
+      <q-separator class="q-mt-md" inset />
+      <q-card-section class="row">
+        <q-card-section class="column justify-start text-h6 q-mt-sm q-mx-md">
+          <span>Items selected: {{ itemsSelected }} </span>
+          <span>Total payable: ${{ totalPayable }} </span>
+        </q-card-section>
+      </q-card-section>
+      <q-card-section class="row justify-end">
+        <q-btn
+          class="tex-gyre-adventor-bold col-2 q-mx-md"
+          style="font-size: medium; width: 30%"
+          @click="confirm"
+          color="primary"
+          >Confirm
+        </q-btn>
       </q-card-section>
     </q-card>
   </q-page>
